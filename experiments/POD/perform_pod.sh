ebaae2f606ab21fdc1146f0557c2f0b55e2f6dd7
Dario Coscia <93731561+dario-coscia@users.noreply.github.com>
2023-05-17 18:18:03 +0200

    Update perform_pod.sh

--- experiments/POD/perform_pod.sh
+++ experiments/POD/perform_pod.sh
@@ -12,15 +12,3 @@ python3 pod_graetz.py --singular_values=1
 echo "LID" 
 echo "##########################"
 python3 pod_lid.py --singular_values=1
-
-# echo "HEAT" 
-# echo "##########################"
-# python3 pod_heat.py
-
-# echo "NAVIER STOKES" 
-# echo "##########################"
-# python3 pod_ns.py
-
-# echo "ELASTIC" 
-# echo "##########################"
-# python3 pod_elasticblock.py
